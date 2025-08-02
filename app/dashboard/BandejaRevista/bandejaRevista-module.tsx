e34d6d29371cfe097be5a51d532927a5144d1c25
antonioDiaz04 <20221136@uthh.edu.mx>
2025-08-02 12:17:01 -0600

    estatus

--- app/dashboard/BandejaRevista/bandejaRevista-module.tsx
+++ app/dashboard/BandejaRevista/bandejaRevista-module.tsx
@@ -500,8 +500,8 @@ export default function BandejaRevistaModule() {
               <SelectValue placeholder="Estatus" />
             </SelectTrigger>
             <SelectContent>
-              <SelectItem value="Registrado">Registrado</SelectItem>
-              <SelectItem value="Impreso">Impreso</SelectItem>
+              {/* <SelectItem value="Registrado">Registrado</SelectItem>
+              <SelectItem value="Impreso">Impreso</SelectItem> */}
               <SelectItem value="all">Todos</SelectItem>
             </SelectContent>
           </Select>
